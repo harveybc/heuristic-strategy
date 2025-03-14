bc2bdd0d0ff43f535b72bbc5eff3e2b94ea17570
Harvey Bastidas <harveybc@ingeni-us.com>
2025-03-14 13:14:00 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,23 +1,12 @@
 # config.py
 
 DEFAULT_VALUES = {
-    #"hourly_predictions_file": "tests/data/phase_2_3_cnn_1h_prediction_d3.csv",
-    #"daily_predictions_file": "tests/data/phase_2_3_cnn_1d_prediction_d3.csv",
-    #"daily_predictions_file": "tests/data/ann_predictions_daily_d3.csv",
-    "hourly_predictions_file": "tests/data/phase_1_transformer_25200_1h_prediction.csv",
-    "daily_predictions_file": "tests/data/phase_1_transformer_25200_1d_prediction.csv",
-    "uncertainty_hourly_file":"tests/data/phase_1_transformer_25200_1h_uncertanties.csv",
-    "uncertainty_daily_file": "tests/data/phase_1_transformer_25200_1d_uncertanties.csv",
-    #"hourly_predictions_file": "tests/data/lstm_predictions_hourly_d3.csv",
-    #"daily_predictions_file": "tests/data/cnn_predictions_daily_d3.csv",
+    #"hourly_predictions_file": "tests/data/phase_1_transformer_25200_1h_prediction.csv",
+    #"daily_predictions_file": "tests/data/phase_1_transformer_25200_1d_prediction.csv",
+    #"uncertainty_hourly_file":"tests/data/phase_1_transformer_25200_1h_uncertanties.csv",
+    #"uncertainty_daily_file": "tests/data/phase_1_transformer_25200_1d_uncertanties.csv",
+    #"base_dataset_file": "tests/data/phase_1_base_d3.csv",
     
-    #"hourly_predictions_file": "tests/data/ideal_predictions_hourly_d3.csv",
-    #"daily_predictions_file": "tests/data/ideal_predictions_daily_d3.csv",
-    #"hourly_predictions_file": None,
-    #"daily_predictions_file":  None,
-    
-    "base_dataset_file": "tests/data/phase_1_base_d3.csv",
-    #"base_dataset_file": "tests/data/phase_1_base_d1.csv",
     "date_column": "DATE_TIME",
     "plugin": "default",
     "time_horizon": 6,
