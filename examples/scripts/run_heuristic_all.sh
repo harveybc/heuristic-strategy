c259559d3986aa65f9b76b66bfd900997f95e1db
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-04-25 05:24:23 -0500

    update

--- examples/scripts/run_heuristic_all.sh
+++ examples/scripts/run_heuristic_all.sh
@@ -4,21 +4,21 @@ CONFIG_DIR="examples/config/phase_1_daily"
 
 for file in "$CONFIG_DIR"/*.json; do
     echo "Running preprocessor with configuration: $(basename "$file")"
-    sh ./.sh heuristic.sh --predictor_daily_config_file "$file" --predictor_hourly_config_file "examples/config/phase_1/phase_1_cnn_25200_1h_config.json" --base_dataset_file "examples/data/phase_1/phase_1_base_d3.csv"
+    sh ./heuristic.sh --predictor_daily_config_file "$file" --predictor_hourly_config_file "examples/config/phase_1/phase_1_cnn_25200_1h_config.json" --base_dataset_file "examples/data/phase_1/phase_1_base_d3.csv"
 done
 
 CONFIG_DIR="examples/config/phase_2_daily"
 
 for file in "$CONFIG_DIR"/*.json; do
     echo "Running preprocessor with configuration: $(basename "$file")"
-    sh ./.sh heuristic.sh --predictor_daily_config_file "$file" --predictor_hourly_config_file "examples/config/phase_2/phase_2_4_cnn_1h_config.json" --base_dataset_file "examples/data/phase_2_4/base_d3.csv"
+    sh ./heuristic.sh --predictor_daily_config_file "$file" --predictor_hourly_config_file "examples/config/phase_2/phase_2_4_cnn_1h_config.json" --base_dataset_file "examples/data/phase_2_4/base_d3.csv"
 done
 
 CONFIG_DIR="examples/config/phase_3_1_daily"
 
 for file in "$CONFIG_DIR"/*.json; do
     echo "Running preprocessor with configuration: $(basename "$file")"
-    sh ./.sh heuristic.sh --predictor_daily_config_file "$file" --predictor_hourly_config_file "examples/config/phase_3_1/phase_3_1_cnn_1h_config.json" --base_dataset_file "examples/data/phase_2_4/base_d6.csv"
+    sh ./heuristic.sh --predictor_daily_config_file "$file" --predictor_hourly_config_file "examples/config/phase_3_1/phase_3_1_cnn_1h_config.json" --base_dataset_file "examples/data/phase_2_4/base_d6.csv"
 done
 
 
--- examples/scripts/run_heuristic_phase_3.sh
+++ examples/scripts/run_heuristic_phase_3.sh
@@ -6,7 +6,7 @@ CONFIG_DIR="examples/config/phase_3_1_daily"
 
 for file in "$CONFIG_DIR"/*.json; do
     echo "Running preprocessor with configuration: $(basename "$file")"
-    sh ./.sh heuristic.sh --predictor_daily_config_file "$file" --predictor_hourly_config_file "examples/config/phase_3_1/phase_3_1_cnn_1h_config.json" --base_dataset_file "examples/data/phase_2_4/base_d6.csv"
+    sh ./heuristic.sh --predictor_daily_config_file "$file" --predictor_hourly_config_file "examples/config/phase_3_1/phase_3_1_cnn_1h_config.json" --base_dataset_file "examples/data/phase_2_4/base_d6.csv"
 done
 
 
