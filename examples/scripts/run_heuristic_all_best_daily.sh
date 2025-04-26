8ecad148e9580a38c676dada8d1af3dd59fceb9e
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-04-26 18:48:48 -0500

    Update run_heuristic_all_best_daily.sh

--- examples/scripts/run_heuristic_all_best_daily.sh
+++ examples/scripts/run_heuristic_all_best_daily.sh
@@ -18,7 +18,7 @@ CONFIG_DIR="examples/config/phase_3_1_daily"
 
 for file in "$CONFIG_DIR"/*.json; do
     echo "Running preprocessor with configuration: $(basename "$file")"
-    sh ./heuristic.sh --predictor_hourly_config_file "$file" --predictor_daily_config_file "examples/config/phase_3_1/phase_3_1_cnn_1h_config.json" --base_dataset_file "examples/data/phase_3/base_d6.csv" --load_parameters "examples/results/phase_3_1_daily/phase_3_1_cnn_25200_1d_results_parameters.json" --prefix "best_daily_"
+    sh ./heuristic.sh --predictor_hourly_config_file "$file" --predictor_daily_config_file "examples/config/phase_3_1/phase_3_1_cnn_1d_config.json" --base_dataset_file "examples/data/phase_3/base_d6.csv" --load_parameters "examples/results/phase_3_1_daily/phase_3_1_cnn_25200_1d_results_parameters.json" --prefix "best_daily_"
 done
 
 
