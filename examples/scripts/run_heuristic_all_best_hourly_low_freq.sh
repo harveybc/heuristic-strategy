f4ca68a7b6432970ba4f8bd4ec016661176df493
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-04-26 23:28:21 -0500

    update

--- examples/scripts/run_heuristic_all_best_hourly_low_freq.sh
+++ examples/scripts/run_heuristic_all_best_hourly_low_freq.sh
@@ -11,7 +11,7 @@ CONFIG_DIR="examples/config/phase_2_daily"
 
 for file in "$CONFIG_DIR"/*.json; do
     echo "Running preprocessor with configuration: $(basename "$file")"
-    sh ./heuristic.sh --predictor_daily_config_file "$file" --predictor_hourly_config_file "examples/config/phase_2/phase_2_4_cnn_1h_config.json" --base_dataset_file "examples/data/phase_2_4/base_d3.csv" --load_parameters "examples/results/phase_2_1_daily/phase_2_1_ann_25200_1d_results_low_frequency_parameters.json" --prefix "_best_hourly_low_freq" --max_trades_per_5days 3
+    sh ./heuristic.sh --predictor_daily_config_file "$file" --predictor_hourly_config_file "examples/config/phase_2/phase_2_2_cnn_1h_config.json" --base_dataset_file "examples/data/phase_2_4/base_d3.csv" --load_parameters "examples/results/phase_2_1_daily/phase_2_1_ann_25200_1d_results_low_frequency_parameters.json" --prefix "_best_hourly_low_freq" --max_trades_per_5days 3
 done
 
 CONFIG_DIR="examples/config/phase_3_1_daily"
