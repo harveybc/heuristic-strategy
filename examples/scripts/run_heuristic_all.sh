ceb039d8a3809fe8bd750c074db91c55de9e2077
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-04-26 19:35:06 -0500

    update

--- examples/scripts/run_heuristic_all.sh
+++ examples/scripts/run_heuristic_all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sh examples/scripts/run_heuristic_all_best_daily.sh
-sh examples/scripts/run_heuristic_all_best_hourly.sh
+sh examples/scripts/run_heuristic_all_best_daily_low_freq.sh
+sh examples/scripts/run_heuristic_all_best_hourly_low_freq.sh
+sh examples/scripts/run_heuristic_all_best_daily_high_freq.sh
+sh examples/scripts/run_heuristic_all_best_hourly_high_freq.sh
 
 echo "All configurations processed."
---
+++ examples/scripts/run_heuristic_all_best_daily_high_freq.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CONFIG_DIR="examples/config/phase_1"
+
+for file in "$CONFIG_DIR"/*.json; do
+    echo "Running preprocessor with configuration: $(basename "$file")"
+    sh ./heuristic.sh --predictor_hourly_config_file "$file" --predictor_daily_config_file "examples/config/phase_1_daily/phase_1_ann_25200_1d_config.json" --base_dataset_file "examples/data/phase_1/phase_1_base_d3.csv" --load_parameters "examples/results/best_phase_3_1_high_freq_trading.json" --prefix "_best_daily_high_freq"
+done
+
+CONFIG_DIR="examples/config/phase_2"
+
+for file in "$CONFIG_DIR"/*.json; do
+    echo "Running preprocessor with configuration: $(basename "$file")"
+    sh ./heuristic.sh --predictor_hourly_config_file "$file" --predictor_daily_config_file "examples/config/phase_2_daily/phase_2_4_ann_1d_config.json" --base_dataset_file "examples/data/phase_2_4/base_d3.csv" --load_parameters "examples/results/best_phase_3_1_high_freq_trading.json" --prefix "_best_daily_high_freq"
+done
+
+CONFIG_DIR="examples/config/phase_3_1"
+
+for file in "$CONFIG_DIR"/*.json; do
+    echo "Running preprocessor with configuration: $(basename "$file")"
+    sh ./heuristic.sh --predictor_hourly_config_file "$file" --predictor_daily_config_file "examples/config/phase_3_1_daily/phase_3_1_cnn_1d_config.json" --base_dataset_file "examples/data/phase_3/base_d6.csv" --load_parameters "examples/results/best_phase_3_1_high_freq_trading.json" --prefix "_best_daily_high_freq"
+done
+
+
+
+echo "All configurations processed."
