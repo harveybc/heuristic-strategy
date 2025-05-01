be7c4565b8606090f02f65a7d304d593fdcde56e
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-01 08:32:45 -0500

    Update run_heuristic_all_high_freq.sh

--- examples/scripts/run_heuristic_all_high_freq.sh
+++ examples/scripts/run_heuristic_all_high_freq.sh
@@ -21,10 +21,10 @@ declare -A HOURLY_CFG_MAP=(
 
 # per‐phase load‐parameters
 declare -A LOAD_PARAMS=(
-    [phase_1]="examples/results/phase_1_daily/phase_1_ann_25200_1d_results_high_frequency_parameters.json"
-    [phase_2]="examples/results/phase_2_daily/phase_2_ann_25200_1d_results_high_frequency_parameters.json"
-    [phase_3_1]="examples/results/phase_3_1_daily/phase_3_1_cnn_25200_1d_results_high_frequency_parameters.json"
-    [phase_3_2]="examples/results/phase_3_2_daily/phase_3_2_cnn_25200_1d_results_high_frequency_parameters.json"
+    [phase_1]="examples/config/phase_1_daily/phase_1_ann_25200_1d_config.json"
+    [phase_2]="examples/config/phase_2_daily/phase_2_4_ann_1d_config.json"
+    [phase_3_1]="examples/config/phase_3_1_daily/phase_3_1_cnn_1d_config.json"
+    [phase_3_2]="examples/config/phase_3_2_daily/phase_3_2_cnn_1d_config.json"
 )
 
 PREFIX="_best_hourly_high_freq"
