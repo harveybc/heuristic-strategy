fafc96e5c9b2d3a8dc9162b2775275d5e31cb934
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-01 08:36:09 -0500

    Update run_heuristic_all.sh

--- examples/scripts/run_heuristic_all.sh
+++ examples/scripts/run_heuristic_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sh examples/scripts/run_heuristic_all_low_freq.sh
-sh examples/scripts/run_heuristic_all_high_freq.sh
+bash examples/scripts/run_heuristic_all_low_freq.sh
+bash examples/scripts/run_heuristic_all_high_freq.sh
 
 
 echo "All configurations processed."
