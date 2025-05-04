33f7335fff9cdbab43b0dc08501531f5c1a28acb
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-04 05:43:51 -0500

    Update plugin_long_short_predictions.py

--- heuristic_strategy_plugins/plugin_long_short_predictions.py
+++ heuristic_strategy_plugins/plugin_long_short_predictions.py
@@ -70,7 +70,7 @@ class Plugin:
 
         # Unpack candidate parameters:
         # (profit_threshold, tp_multiplier, sl_multiplier, lower_rr_threshold, upper_rr_threshold, time_horizon)
-        profit_threshold, tp_multiplier, sl_multiplier, lower_rr, upper_rr, time_horizon = individual
+        profit_threshold, tp_multiplier, sl_multiplier, lower_rr, upper_rr = individual
 
         # Check that predictions are available.
         if (hourly_predictions is None or hourly_predictions.empty or 
