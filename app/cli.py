3b30dd134a478db52676f68b48c75fed4a5c4d61
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-04 17:13:45 -0500

    update

--- app/cli.py
+++ app/cli.py
@@ -41,6 +41,8 @@ def parse_args():
     parser.add_argument('--force_date', action='store_true', help='Include date in the output CSV files.')
     parser.add_argument('--headers', action='store_true', help='Indicate if the CSV files have headers.')
 
+    parser.add_argument('--use_first_match', action='store_true', help='Use first match  or use best match for opening.')
+
     # "feature_extractor_file": "examples/results/phase_3_2_daily/phase_3_2_cnn_25200_1d_encoder_model.h5.keras",
     # "train_fe" : false
     parser.add_argument('--feature_extractor_file', type=str, help='Path to the feature extractor file.')
