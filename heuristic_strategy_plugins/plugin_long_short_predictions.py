21fe95dc210105126b7d7cc6a02f7d97e9f93f0c
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-04 08:09:08 -0500

    Update plugin_long_short_predictions.py

--- heuristic_strategy_plugins/plugin_long_short_predictions.py
+++ heuristic_strategy_plugins/plugin_long_short_predictions.py
@@ -352,9 +352,9 @@ class Plugin:
             ideal_drawdown_pips_buy = (profit_pred - min_before_max) / self.p.pip_cost
             
             #rr_buy = ideal_profit_pips_buy / ideal_drawdown_pips_buy if ideal_drawdown_pips_buy > 0 else 0
-            #rr_buy = (ideal_profit_pips_buy - ideal_drawdown_pips_buy )/(max_idx+1)
+            rr_buy = (ideal_profit_pips_buy - ideal_drawdown_pips_buy )/(max_idx+1)
             #rr_buy = ideal_profit_pips_buy 
-            rr_buy = ideal_profit_pips_buy / (max_idx+1)
+            #rr_buy = ideal_profit_pips_buy / (max_idx+1)
 
             tp_buy = current_price + self.p.tp_multiplier * ideal_profit_pips_buy * self.p.pip_cost
             sl_buy = current_price - self.p.sl_multiplier * ideal_drawdown_pips_buy * self.p.pip_cost
@@ -374,9 +374,9 @@ class Plugin:
 
 
             #rr_sell = ideal_profit_pips_sell / ideal_drawdown_pips_sell if ideal_drawdown_pips_sell > 0 else 0
-            #rr_sell = (ideal_profit_pips_sell - ideal_drawdown_pips_sell)/(min_idx+1)
+            rr_sell = (ideal_profit_pips_sell - ideal_drawdown_pips_sell)/(min_idx+1)
             #rr_sell = ideal_profit_pips_sell
-            rr_sell = ideal_profit_pips_sell/ (min_idx+1)
+            #rr_sell = ideal_profit_pips_sell/ (min_idx+1)
             
             tp_sell = current_price - self.p.tp_multiplier * ideal_profit_pips_sell * self.p.pip_cost
             sl_sell = current_price + self.p.sl_multiplier * ideal_drawdown_pips_sell * self.p.pip_cost
