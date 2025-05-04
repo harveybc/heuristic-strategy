016b33a3b29d56e85562d5b3351712618900a2bb
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-04 17:07:34 -0500

    Revert "update"
    This reverts commit f1c4a02e17943709cf3278c6adae0aae10d53adb.

--- app/config.py
+++ app/config.py
@@ -54,8 +54,7 @@ DEFAULT_VALUES = {
     "daily_columns": ["Prediction_H24", "Prediction_H48", "Prediction_H72", "Prediction_H96", "Prediction_H120", "Prediction_H144"],
     "hourly_columns": ["Prediction_H1", "Prediction_H2", "Prediction_H3", "Prediction_H4", "Prediction_H5", "Prediction_H6"],
     "uncertainty_daily_columns": ["Uncertainty_H24", "Uncertainty_H48", "Uncertainty_H72", "Uncertainty_H96", "Uncertainty_H120", "Uncertainty_H144"],
-    "uncertainty_hourly_columns": ["Uncertainty_H1", "Uncertainty_H2", "Uncertainty_H3", "Uncertainty_H4", "Uncertainty_H5", "Uncertainty_H6"],
-    "use_first_match": False
+    "uncertainty_hourly_columns": ["Uncertainty_H1", "Uncertainty_H2", "Uncertainty_H3", "Uncertainty_H4", "Uncertainty_H5", "Uncertainty_H6"]
     
      
 }
