c36bf947a7288a6219616ab123e588221cc670fa
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-05 07:54:50 -0500

    update

--- examples/scripts/run_heuristic_all_low_freq.sh
+++ examples/scripts/run_heuristic_all_low_freq.sh
@@ -14,10 +14,10 @@ declare -A BASE_DATA_MAP=(
 
 # per‐phase load‐parameters
 declare -A LOAD_PARAMS=(
-    [phase_1]="examples/results/phase_3_1_daily/phase_3_1_cnn_25200_1d_results_low_frequency_parameters.json"
-    [phase_2]="examples/results/phase_3_1_daily/phase_3_1_cnn_25200_1d_results_low_frequency_parameters.json"
-    [phase_3_1]="examples/results/phase_2_1_daily/phase_2_1_ann_25200_1d_results_low_frequency_parameters.json"
-    [phase_3_2]="examples/results/phase_2_1_daily/phase_2_1_ann_25200_1d_results_low_frequency_parameters.json"
+    [phase_1]="examples/results/phase_2_1_daily/phase_2_1_ann_25200_1d_results_low_frequency_parameters.json"
+    [phase_2]="examples/results/phase_2_1_daily/phase_2_1_ann_25200_1d_results_low_frequency_parameters.json"
+    [phase_3_1]="examples/results/phase_3_1_daily/phase_3_1_cnn_25200_1d_results_low_frequency_parameters.json"
+    [phase_3_2]="examples/results/phase_3_1_daily/phase_3_1_cnn_25200_1d_results_low_frequency_parameters.json"
 )
 
 PREFIX="_low_freq"
