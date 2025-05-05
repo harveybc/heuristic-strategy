bf0e191fbd77a63703b0fcf063886df58ab5d480
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-05 12:07:08 -0500

    Update optimize_best.sh

--- examples/scripts/optimize_best.sh
+++ examples/scripts/optimize_best.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bash heuristic.sh --predictor_daily_config_file examples/config/phase_2_daily/phase_2_1_ann_1d_config.json --predictor_hourly_config_file examples/config/phase_2/phase_2_1_cnn_1h_config.json --base_dataset_file examples/data/phase_2_1/base_d3.csv --prefix _high_frequency --max_trades_per_5days 20
-bash heuristic.sh --predictor_daily_config_file examples/config/phase_3_1_daily/phase_3_1_cnn_1d_config.json --predictor_hourly_config_file examples/config/phase_3_1/phase_3_1_cnn_1h_config.json --base_dataset_file examples/data/phase_3/base_d6.csv --prefix _high_frequency --max_trades_per_5days 20
+bash heuristic.sh --predictor_daily_config_file examples/config/phase_3_2_daily/phase_3_2_cnn_1d_config.json --predictor_hourly_config_file examples/config/phase_3_2/phase_3_2_transformer_1h_config.json --base_dataset_file examples/data/phase_3/base_d6.csv --prefix _high_frequency --max_trades_per_5days 20
 bash heuristic.sh --predictor_daily_config_file examples/config/phase_2_daily/phase_2_1_ann_1d_config.json --predictor_hourly_config_file examples/config/phase_2/phase_2_1_cnn_1h_config.json --base_dataset_file examples/data/phase_2_1/base_d3.csv --prefix _low_frequency --max_trades_per_5days 3
-bash heuristic.sh --predictor_daily_config_file examples/config/phase_3_1_daily/phase_3_1_cnn_1d_config.json --predictor_hourly_config_file examples/config/phase_3_1/phase_3_1_cnn_1h_config.json --base_dataset_file examples/data/phase_3/base_d6.csv --prefix _low_frequency --max_trades_per_5days 3
+bash heuristic.sh --predictor_daily_config_file examples/config/phase_3_2_daily/phase_3_2_cnn_1d_config.json --predictor_hourly_config_file examples/config/phase_3_2/phase_3_2_transformer_1h_config.json --base_dataset_file examples/data/phase_3/base_d6.csv --prefix _low_frequency --max_trades_per_5days 3
 bash examples/scripts/run_heuristic_all.sh
 echo "All configurations processed."
