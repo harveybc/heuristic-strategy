c7480cfdd63768b03fac6cee7f7f1c3c605d3747
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-11-16 07:09:54 -0500

    Update olap_noise_load_results.sh

--- examples/scripts/olap_noise_load_results.sh
+++ examples/scripts/olap_noise_load_results.sh
@@ -1,3 +1,3 @@
 export NOISE_OLAP_DB_URL="postgresql+psycopg2://metabase:metabase_pass@localhost:5432/noise_sensitivity_olap"
 
-python examples/olap/load_noise_sensitivity_results.py ./results/noise_sensitivity
+python olap/load_noise_sensitivity_results.py ./examples/results/noise_sensitivity
