b7c214e4b53b60f75b4a4cc620d38f3052dbc60f
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-11-18 07:26:51 -0500

    Update neat_optimizer.py

--- heuristic_optimizer_plugins/neat_optimizer.py
+++ heuristic_optimizer_plugins/neat_optimizer.py
@@ -32,7 +32,7 @@ class Plugin:
         "stagnation_limit": 15,
         "weight_mutation_power": 2.5,
         "bias_mutation_power": 0.5,
-        "compatibility_threshold": 1.1,
+        "compatibility_threshold": 1.6,
         "activation_default": "tanh",
         "aggregation_default": "sum",
         "no_fitness_termination": False,
