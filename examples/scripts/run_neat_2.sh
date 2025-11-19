143c29d23f58d8384299d092f5f53fbb18282601
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-11-18 23:43:51 -0500

    update

--- examples/scripts/run_neat_2.sh
+++ examples/scripts/run_neat_2.sh
@@ -1,10 +1,7 @@
+bash heuristic.sh --prefix _high_frequency --max_trades_per_5days 20 --gaussian_noise_mean 0.0028 --gaussian_noise_stddev 0.0028 --optimizer_plugin neat_optimizer
 bash heuristic.sh --prefix _high_frequency --max_trades_per_5days 20 --gaussian_noise_mean 0.0030 --gaussian_noise_stddev 0.0030 --optimizer_plugin neat_optimizer
 bash heuristic.sh --prefix _high_frequency --max_trades_per_5days 20 --gaussian_noise_mean 0.0032 --gaussian_noise_stddev 0.0032 --optimizer_plugin neat_optimizer
 bash heuristic.sh --prefix _high_frequency --max_trades_per_5days 20 --gaussian_noise_mean 0.0035 --gaussian_noise_stddev 0.0035 --optimizer_plugin neat_optimizer
-bash heuristic.sh --prefix _high_frequency --max_trades_per_5days 20 --gaussian_noise_mean 0.0036 --gaussian_noise_stddev 0.0036 --optimizer_plugin neat_optimizer
-bash heuristic.sh --prefix _high_frequency --max_trades_per_5days 20 --gaussian_noise_mean 0.0037 --gaussian_noise_stddev 0.0037 --optimizer_plugin neat_optimizer
-bash heuristic.sh --prefix _high_frequency --max_trades_per_5days 20 --gaussian_noise_mean 0.0038 --gaussian_noise_stddev 0.0038 --optimizer_plugin neat_optimizer
-
 
 
 
